78c971fc9d9c2ca093292467ca66b20334be066c
tomdaniels <thomsondaniels@hotmail.com>
2018-02-05 18:16:09 +1100

    playing with store.dispatch();

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,8 +2,40 @@ import { createStore } from 'redux';
 
 // have to pass createStore a function as an argument, the functions first argument will always be "state";
 
-const store = createStore((state = { count: 0 }) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + 1
+            };
+        case 'DECREMENT':
+            return {
+              count: state.count - 1
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+                return state;
+    }
+});
+
+// Actions - object that gets sent to the store.
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'RESET'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
 });
 
 console.log(store.getState());
