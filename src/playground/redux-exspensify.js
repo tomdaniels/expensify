d9a89c06a4c008d6eedb8c2e02aa1bf84bf57760
tomdaniels <thomsondaniels@hotmail.com>
2018-02-09 15:00:44 +1100

    filters first reducer

--- src/playground/redux-exspensify.js
+++ src/playground/redux-exspensify.js
@@ -34,6 +34,12 @@ const editExpense = (id, updates) => ({
 });
 
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+   type: 'SET_TEXT_FILTER',
+   text
+});
+
+
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -79,6 +85,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            };
         default:
             return state;
     }
@@ -103,6 +114,10 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
+
 
 const demoState = {
     expenses: [{
