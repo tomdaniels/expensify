ab23ec8fd1b0cc91c6c42c4d2e7778a77f4f534c
tomdaniels <thomsondaniels@hotmail.com>
2018-02-12 10:56:42 +1100

    redux file structure

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routers/app-router';
+import configureStore from './store/configure-store';
+import { addExpense } from './actions/expenses';
+import getVisibleExpenses from './selectors/expenses';
+import { setTextFilter } from './actions/filters';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
+const store = configureStore();
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 80000 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 30000 }));
+store.dispatch(setTextFilter('water'));
+
+
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
 
