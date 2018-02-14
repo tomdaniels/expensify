07071c0e9d946d83f0cce4a3ac31dcd7a1ffe732
tomdaniels <thomsondaniels@hotmail.com>
2018-02-14 21:08:01 +1100

    filter action generator tests

---
+++ src/test/actions/filters.test.js
@@ -0,0 +1,54 @@
+import moment from 'moment';
+import {
+    setStartDate,
+    setEndDate,
+    sortByAmount,
+    sortByDate,
+    setTextFilter
+} from '../../actions/filters';
+
+test('should generate set start date action object', () => {
+    const action = setStartDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_START_DATE',
+        startDate: moment(0)
+    });
+});
+
+test('should generate set end date action object', () => {
+    const action = setEndDate(moment(0));
+    expect(action).toEqual({
+        type: 'SET_END_DATE',
+        endDate: moment(0)
+    });
+});
+
+test('should generate sort by amount action object', () => {
+   const action = sortByAmount();
+   expect(action).toEqual({
+       type: 'SORT_BY_AMOUNT'
+   });
+});
+
+test('should generate sort by date action object', () => {
+    const action = sortByDate();
+    expect(action).toEqual({
+        type: 'SORT_BY_DATE'
+    });
+});
+
+test('should set up text filter action object as empty string with no data', () => {
+    const action = setTextFilter();
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: ''
+    });
+});
+
+test('should set up text filter action object as empty string with supplied text', () => {
+    const action = setTextFilter('rent');
+    expect(action).toEqual({
+        type: 'SET_TEXT_FILTER',
+        text: 'rent'
+    });
+});
