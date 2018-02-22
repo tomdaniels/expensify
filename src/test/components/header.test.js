1380abd8ff340abe7d4bcb1736816d1b33338de9
tomdaniels <thomsondaniels@hotmail.com>
2018-02-22 22:02:34 +1100

    setup enzyme-to-json

--- src/test/components/header.test.js
+++ src/test/components/header.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import Header from '../../components/header';
 
 test('should renderer header', () => {
     const wrapper = shallow(<Header />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
 });
 
