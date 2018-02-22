d486c40d9e0a56d7f0e4fd21852a15f4302faed5
tomdaniels <thomsondaniels@hotmail.com>
2018-02-22 21:55:08 +1100

    change shallow rendering to enzyme

--- src/test/components/header.test.js
+++ src/test/components/header.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/header';
 
 test('should renderer header', () => {
-    const renderer = new ReactShallowRenderer();
-    renderer.render(<Header />);
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('h1').text()).toBe('Expensify');
 });
 
