e774ca1748f9ef77faf6a52ebb4d9513cc618c6c
tomdaniels <thomsondaniels@hotmail.com>
2018-02-25 14:39:36 +1100

    testing input on change

--- src/test/components/expense-form.test.js
+++ src/test/components/expense-form.test.js
@@ -22,3 +22,12 @@ test('should render error for invalid form submit', () => {
     expect(wrapper.state('errorState').length).toBeGreaterThan(0);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('should set description on input change', () => {
+    const value = 'New description';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('input').at(0).simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('description')).toBe(value);
+});
