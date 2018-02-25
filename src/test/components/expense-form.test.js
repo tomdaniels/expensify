605a01f9040c4fbfb29139350795f3aecf1dcd9e
tomdaniels <thomsondaniels@hotmail.com>
2018-02-25 14:42:22 +1100

    on note change testing

--- src/test/components/expense-form.test.js
+++ src/test/components/expense-form.test.js
@@ -31,3 +31,12 @@ test('should set description on input change', () => {
     });
     expect(wrapper.state('description')).toBe(value);
 });
+
+test('should set note on textarea change', () => {
+    const value = 'new note';
+    const wrapper = shallow(<ExpenseForm />);
+    wrapper.find('textarea').simulate('change', {
+        target: { value }
+    });
+    expect(wrapper.state('note')).toBe(value)
+});
