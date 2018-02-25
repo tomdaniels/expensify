a4cec87b8246d7699d1262c1306e88059428af08
tomdaniels <thomsondaniels@hotmail.com>
2018-02-25 13:53:24 +1100

    refactor

--- src/test/components/expense-list-item.test.js
+++ src/test/components/expense-list-item.test.js
@@ -4,12 +4,6 @@ import ExpenseListItem from '../../components/expense-list-item';
 import expenses from '../fixtures/expenses';
 
 test('should render expenses data', () => {
-    const wrapper = shallow(
-        <ExpenseListItem
-            id={expenses[0].id}
-            description={expenses[0].description}
-            amount={expenses[0].amount}
-            createdAt={expenses[0].createdAt}
-        />);
+    const wrapper = shallow(<ExpenseListItem {...expenses[0]}/>);
     expect(wrapper).toMatchSnapshot();
 });
