c0ad910841bb6e48019901ec5077c02906e51a8b
tomdaniels <thomsondaniels@hotmail.com>
2018-02-25 14:26:04 +1100

    expense from snapshot

--- src/test/components/expense-form.test.js
+++ src/test/components/expense-form.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/expense-form';
+import expenses from '../fixtures/expenses';
 
 test('should render the expense from correctly', () => {
     const wrapper = shallow(<ExpenseForm />);
     expect(wrapper).toMatchSnapshot();
 });
 
+test('should render ExpenseForm  correctly with expense data',() => {
+    const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
+    expect(wrapper).toMatchSnapshot();
+});
