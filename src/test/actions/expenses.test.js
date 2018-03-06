05c3deedadb92b878aee5cfce305b136dcaefba7
tomdaniels <thomsondaniels@hotmail.com>
2018-03-06 11:59:17 +1100

    refactor

--- src/test/actions/expenses.test.js
+++ src/test/actions/expenses.test.js
@@ -33,10 +33,10 @@ test('should add expense to database and store', (done) => {
            }
        });
 
-       database.ref(`expenses/${actions[0].expense.id}`).once('value').then((snapshot) => {
-           expect(snapshot.val()).toEqual(expenseData);
-           done();
-       });
+       return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseData);
+        done();
     });
 });
 
