25ac3ff458a9fb2b0cd9c616c1d10aa717329643
tomdaniels <thomsondaniels@hotmail.com>
2018-03-07 00:47:53 +1100

    loading page component and styles

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import { firebase } from './firebase/firebase';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
+import LoadingPage from './components/loading-page.js';
 
 const store = configureStore();
 const jsx = (
@@ -24,7 +25,7 @@ const renderApp = () => {
     }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
    if (user) {
