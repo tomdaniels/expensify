177173043365cd4fcb2db2d866c63103c0b1e00c
tomdaniels <thomsondaniels@hotmail.com>
2018-03-07 00:15:59 +1100

    list header styles

--- src/components/expense-list-item.js
+++ src/components/expense-list-item.js
@@ -4,14 +4,18 @@ import moment from 'moment';
 import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt }) => (
-    <div>
-        <Link to={`/edit/${id}`}>{description}</Link>
-        <p>
-            {numeral(amount / 100).format('$0,0.00')}
-            -
-            {moment(createdAt).format('Do MMMM, YYYY')}
-        </p>
-    </div>
+    <Link
+        className="list-item"
+        to={`/edit/${id}`}
+    >
+        <div>
+            <h3>{description}</h3>
+            <span>{moment(createdAt).format('Do MMMM, YYYY')}</span>
+        </div>
+        <div>
+            <h3>{numeral(amount / 100).format('$0,0.00')}</h3>
+        </div>
+    </Link>
 );
 
 
