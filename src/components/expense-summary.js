108189e343ee152a67a43e5c5c1997bb297468cf
tomdaniels <thomsondaniels@hotmail.com>
2018-03-06 23:09:26 +1100

    styling page headers

--- src/components/expense-summary.js
+++ src/components/expense-summary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import numeral from 'numeral';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpenseTotal from '../selectors/expenses-total';
 
@@ -9,8 +10,13 @@ export const ExpenseSummary = ({ expenseCount, total }) => {
     const formattedExpensesTotal = numeral(total / 100).format('$0,0.00');
 
     return (
-        <div>
-            <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
+        <div className="page-header">
+            <div className="content-container">
+                <h1 className="page_header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                <div className="page-header__actions">
+                    <Link className="button" to="/create">Add Expenses</Link>
+                </div>
+            </div>
         </div>
     )
 };
