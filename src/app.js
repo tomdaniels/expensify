e7d36730da0e0a28809a893adfcad72ab8d64368
tomdaniels <thomsondaniels@hotmail.com>
2018-03-06 20:42:05 +1100

    auth reducer/action & testing

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import AppRouter, { history } from './routers/app-router';
 import configureStore from './store/configure-store';
 import { startSetExpenses } from './actions/expenses';
+import { login, logout } from './actions/auth';
 import { firebase } from './firebase/firebase';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
@@ -27,6 +28,7 @@ ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
 firebase.auth().onAuthStateChanged((user) => {
    if (user) {
+       store.dispatch(login(user.uid));
        store.dispatch(startSetExpenses()).then(() => {
           renderApp();
           if (history.location.pathname === '/') {
@@ -34,6 +36,7 @@ firebase.auth().onAuthStateChanged((user) => {
           }
        });
    } else {
+       store.dispatch(logout());
        renderApp();
        history.push('/');
    }
