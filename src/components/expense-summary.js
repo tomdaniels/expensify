8c1b9044fb8a11611f9d6dd80506003d7154bfad
tomdaniels <thomsondaniels@hotmail.com>
2018-03-08 23:16:34 +1100

    show count of hidden expenses

--- src/components/expense-summary.js
+++ src/components/expense-summary.js
@@ -4,15 +4,18 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import selectExpenses from '../selectors/expenses';
 import selectExpenseTotal from '../selectors/expenses-total';
+import hiddenExpensesCount from '../selectors/hidden-expenses'
 
-export const ExpenseSummary = ({ expenseCount, total }) => {
+export const ExpenseSummary = ({ expenseCount, total, hidden }) => {
     const expenseWord = expenseCount.length === 1 ? 'expense' : 'expenses';
+    const hiddenWord = hidden === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(total / 100).format('$0,0.00');
 
     return (
         <div className="page-header">
             <div className="content-container">
                 <h1 className="page_header__title">Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span></h1>
+                <h4 className="page_header__title subtitle"><span>{hidden}</span> hidden {hiddenWord}</h4>
                 <div className="page-header__actions">
                     <Link className="button" to="/create">Add Expenses</Link>
                 </div>
@@ -27,6 +30,7 @@ const mapStateToProps = state => {
     return {
         expenseCount: visibleExpenses.length,
         total: selectExpenseTotal(visibleExpenses),
+        hidden: hiddenExpensesCount(state.expenses, visibleExpenses)
     }
 };
 
